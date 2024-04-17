a0e0724e4f7a67ef94a8dcd711ac902825a93559
abraham <abesihomb@gmail.com>
2024-04-17 19:02:21 +0700

    first commit

---
+++ src/main/java/com/adproa3/microservicevoucher/MicroserviceVoucherApplication.java
@@ -0,0 +1,13 @@
+package com.adproa3.microservicevoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceVoucherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceVoucherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adproa3/microservicevoucher/MicroserviceVoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adproa3.microservicevoucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceVoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
